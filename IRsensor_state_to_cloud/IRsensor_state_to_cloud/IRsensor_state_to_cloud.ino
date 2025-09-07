285368b8c76fcf675c36246f8ceb4c80cf90e78d
Prathik-lgp <prathik.reddygari_lgp25@ashoka.edu.in>
2025-09-07 12:04:50 +0530

    Upload IR sensor state to cloud

---
+++ IRsensor_state_to_cloud/IRsensor_state_to_cloud/IRsensor_state_to_cloud.ino
@@ -0,0 +1,20 @@
+#include <NinjaIoT.h>
+#include "entry3.h"
+
+NinjaIoT iot;
+
+void setup()
+{
+  Serial.begin(115200);
+  iot.connect(wifiuser, pass, uid); // link: https://iot.roboninja.in/
+}
+
+void loop()
+{
+  iot.ReadAll(); // Read all values from the cloud
+
+  // Read button state on D0 and upload it
+  iot.SyncIN("D2");
+
+  delay(50); // wait 50 milliseconds
+}
